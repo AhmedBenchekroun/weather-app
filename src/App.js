13a4a9d1dd92f5936010bbd7873fc959effb053e
Ahmed Benchekroun <ahmed@chatterresearch.com>
2018-08-21 16:42:35 -0400

    Added validation to prevent user from submitting empty form

--- src/App.js
+++ src/App.js
@@ -7,13 +7,36 @@ const API_KEY = "da2ec68c3f24e6a1975eeb545407936e";
 
 
 class App extends React.Component {
+
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined
+    }
     getWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
-        const apiCall = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + ','+ country + '&appid=' + API_KEY + "&units=metrics");
-        const data = await apiCall.json();
-        console.log(JSON.stringify(data));
+        if (city && country) {
+            const apiCall = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + ','+ country + '&appid=' + API_KEY + "&units=metrics");
+            const data = await apiCall.json();
+            console.log(data);
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                error: ""
+            });
+        } else {
+            this.setState({
+                error: "Please enter a valid name"
+            })
+        }
     }
 
     render() {
@@ -21,7 +44,14 @@ class App extends React.Component {
             <div>
                 <Titles />
                 <Form getWeather={this.getWeather}/>
-                <Weather/>
+                <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                />
             </div>
         );
     }
